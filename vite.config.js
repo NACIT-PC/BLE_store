fedd8b483c2a157d620db43a53cca6b11908ab42
Daniel P. Mgawi <pdmgawi@gmail.com>
2024-11-13 17:45:19 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 
 export default defineConfig({
-    // Your config options here
-    root: './',
-    publicDir: 'public'
+	// Your config options here
+	root: './', publicDir: 'public'
 })
