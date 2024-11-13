bb3a8257277bd6ae4f775f658204f5370f54c0de
Daniel P. Mgawi <pdmgawi@gmail.com>
2024-11-13 17:45:13 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
-import './style.css'
+import {createClient} from '@supabase/supabase-js'
 
-console.log('Vite is running!')
+const supabaseUrl = 'https://mtrxvdvifoxsfhyhcyzc.supabase.co'
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function getProduct(uuid) {
+	try {
+		const {data: Products, error} = await supabase
+			.from('Products')
+			.select("name,uuid,category,cost,description").eq("uuid", uuid)
+		
+		console.log(Products)
+		
+		return Products[0];
+	} catch (e) {
+		throw e;
+	}
+}
+
+export {supabase, getProduct}
